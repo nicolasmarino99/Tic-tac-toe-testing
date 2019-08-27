1f435e7564285f79ee94a0b481dc7fe33c202525
Mauricio Robayo <rfmajo@gmail.com>
2019-08-27 15:43:39 -0500

    Check if a position is valid player#get_position

--- lib/player.rb
+++ lib/player.rb
@@ -8,17 +8,17 @@ class Player
   end
 
   def move(position, board)
-
-    if position.between?(1, 9)
-      board[position - 1] = @mark   
-    else   
-    false
-    end
+    return position.between?(1, 9) ? board[position - 1] = @mark : false
   end
 
   def get_position(board)
     print "Player '#{@name}' move: "
     position = gets.chomp.to_i
+    while !position.between?(1, 9)
+      puts 'That not a valid position!'
+      print 'Chose another position: '
+      position = gets.chomp.to_i
+    end
     while board[position - 1]
       puts 'That position is taken!'
       print 'Chose another position: '
