fd946b565362ac15583be84d0a1b44586f6fc220
Mauricio Robayo <rfmajo@gmail.com>
2019-08-27 14:32:00 -0500

    Add tests for board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,40 @@
 require "board.rb"
 
-describe Board do
+RSpec.describe Board do
+
   describe "#winner?" do
-    it "returns the printed board" do
-      printed_board = Board.new
-      expect(Board.print).to eql()
+    it "returns false for a non winning board" do
+      expect(subject.winner?("O")).to eql(false)
+    end
+    it "returns true for a winning row" do
+      subject.board = ["O", "O", "O",
+                     nil, nil, nil,
+                     nil, nil, nil]
+      expect(subject.winner?("O")).to eql(true)
+    end
+    it "returns true for a winning col" do
+      subject.board = [nil, "O", nil,
+                     nil, "O", nil,
+                     nil, "O", nil]
+      expect(subject.winner?("O")).to eql(true)
+    end
+    it "returns true for a winning diagonal" do
+      subject.board = ["O", nil, nil,
+                     nil, "O", nil, 
+                     nil, nil, "O"]
+      expect(subject.winner?("O")).to eql(true)
     end
   end
+  
   describe "#tie?" do
-    it "returns the printed board" do
-      printed_board = Board.new
-      expect(Board.print).to eql()
+    it "returns false for a non tie" do
+      expect(subject).not_to be_tie
+    end
+    it "returns false for a tie" do
+      subject.board = ["O", "X", "O",
+                    "X", "X", "O", 
+                    "X", "O", "X"]
+      expect(subject).to be_tie
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,15 +2,21 @@ require "player.rb"
 
 describe Player do
   describe "#move" do
-    it "it receive the user´s inputs and restricts its inputs by the game rules" do
-      board = (1..9).to_a
+    it "updates the board with the move of one player" do
+      board = Array.new(9, nil)
+      player1 = Player.new("p1", "X")
+      player1.move(7, board)
+      expect(board).to eql([nil, nil, nil, nil, nil, nil, "X", nil, nil])
+    end
+    it "updates the board with multiple moves from two players" do
+      board = Array.new(9, nil)
       player1 = Player.new("p1", "X")
       player2 = Player.new("p2", "O")
       player1.move(1, board)
       player2.move(4, board)
       player1.move(2, board)
       player2.move(5, board)
-      expect(board).to eql(["X", "X", 3, "O", "O", 6, 7, 8, 9])
+      expect(board).to eql(["X", "X", nil, "O", "O", nil, nil, nil, nil])
     end
   end
 end
