70a6fbaec8be7b22d00a964ca0827dea43766d61
Mauricio Robayo <rfmajo@gmail.com>
2019-08-28 10:45:36 -0500

    Add spaces between examples(rspec)

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,27 +3,32 @@
 require 'board.rb'
 
 RSpec.describe Board do
+  subject(:board) { described_class.new }
+
   describe '#winner?' do
     it 'returns false for a non winning board' do
-      expect(subject.winner?('O')).to eql(false)
+      expect(board.winner?('O')).to be(false)
     end
+
     it 'returns true for a winning row' do
-      subject.board = ['O', 'O', 'O',
-                       nil, nil, nil,
-                       nil, nil, nil]
-      expect(subject.winner?('O')).to eql(true)
+      board.board = ['O', 'O', 'O',
+                     nil, nil, nil,
+                     nil, nil, nil]
+      expect(board.winner?('O')).to be(true)
     end
+
     it 'returns true for a winning col' do
-      subject.board = [nil, 'O', nil,
-                       nil, 'O', nil,
-                       nil, 'O', nil]
-      expect(subject.winner?('O')).to eql(true)
+      board.board = [nil, 'O', nil,
+                     nil, 'O', nil,
+                     nil, 'O', nil]
+      expect(board.winner?('O')).to be(true)
     end
+
     it 'returns true for a winning diagonal' do
-      subject.board = ['O', nil, nil,
-                       nil, 'O', nil,
-                       nil, nil, 'O']
-      expect(subject.winner?('O')).to eql(true)
+      board.board = ['O', nil, nil,
+                     nil, 'O', nil,
+                     nil, nil, 'O']
+      expect(board.winner?('O')).to be(true)
     end
   end
 
@@ -32,7 +37,7 @@ RSpec.describe Board do
       expected_string = " \e[37m1\e[0m | \e[37m2\e[0m | \e[37m3\e[0m\n---+---+---\n"
       expected_string += " \e[37m4\e[0m | \e[37m5\e[0m | \e[37m6\e[0m\n---+---+---\n"
       expected_string += " \e[37m7\e[0m | \e[37m8\e[0m | \e[37m9\e[0m"
-      expect(subject.to_s).to eql(expected_string)
+      expect(board.to_s).to eql(expected_string)
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,23 +5,23 @@ require 'player.rb'
 describe Player do
   describe '#move' do
     let(:board) { Array.new(9, nil) }
-    let(:player1) { Player.new('p1', 'X') }
-    let(:player2) { Player.new('p2', 'O') }
+    let(:player1) { described_class.new('p1', 'X') }
+    let(:player2) { described_class.new('p2', 'O') }
 
     it 'updates the board with the move of one player' do
       player1.move(7, board)
       expect(board).to eql([nil, nil, nil, nil, nil, nil, 'X', nil, nil])
     end
+    
     it 'updates the board with multiple moves from two players' do
       board = Array.new(9, nil)
       player1.move(1, board)
       player2.move(4, board)
-      player1.move(2, board)
-      player2.move(5, board)
-      expect(board).to eql(['X', 'X', nil, 'O', 'O', nil, nil, nil, nil])
+      expect(board).to eql(['X', nil, nil, 'O', nil, nil, nil, nil, nil])
     end
+
     it 'returns false if the input is out of boundaries' do
-      expect(player1.move(100, board)).to eql(false)
+      expect(player1.move(100, board)).to be(false)
     end
   end
 end
